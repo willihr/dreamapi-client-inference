5eaf1b75e4cab0b5c6c6f3683c5d788e06046956
willihr <willihr10@gmail.com>
2022-12-05 00:19:44 -0300

    async mutex install

--- workers/ai-inferer.js
+++ workers/ai-inferer.js
@@ -1,7 +1,10 @@
-const fs = require("fs");
+const fs = require('fs');
 const LRU = require('lru-cache');
+const { Semaphore } = 'async-mutex';
 
 const downloadFromS3 = require('../utils/download-from-s3');
+
+const gpuSemaphore = new Semaphore(process.env.INFER_GPU_JOBS_MAX_CONCURRENCY || 1);
 
 const modelsCache = new LRU({
     maxSize: process.env.AI_MODEL_CACHE_MAX_MB || 10 * 1024,
@@ -22,6 +25,9 @@ const modelsCache = new LRU({
 
 const aiInferer = async (jobData) => {
     const ckptPath = await modelsCache.fetch(jobData.model_id);
+    await gpuSemaphore.runExclusive(async () => {
+        
+    });
 }
 
 module.exports = aiInferer;
